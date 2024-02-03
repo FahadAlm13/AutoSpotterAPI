644269aeae2cec43f831a93ff6698abe9ffe1ac8
fahadalm13 <f.a.a.m900@gmail.com>
2024-02-03 22:37:53 +0300

    updte

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -3,14 +3,14 @@ import * as firebase from "firebase/app";
 import { initializeFirestore, CACHE_SIZE_UNLIMITED, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo6fgi3GLw1I3XBDwu3ZU1mbWmS2DFhZM",
-    authDomain: "autospotter-ad598.firebaseapp.com",
-    databaseURL: "https://autospotter-ad598-default-rtdb.firebaseio.com",
-    projectId: "autospotter-ad598",
-    storageBucket: "autospotter-ad598.appspot.com",
-    messagingSenderId: "928109315099",
-    appId: "1:928109315099:web:0d80307892b662362c24be"
-};
+    apiKey: "AIzaSyDBLC9fhnadHS8oGKrbvwQuCJ-c1RHNtbE",
+    authDomain: "autospotter-290bc.firebaseapp.com",
+    projectId: "autospotter-290bc",
+    storageBucket: "autospotter-290bc.appspot.com",
+    messagingSenderId: "805115554900",
+    appId: "1:805115554900:web:395a5c263436afc4e991c1"
+  };
+  
 
 
 const app = firebase.initializeApp(firebaseConfig);
